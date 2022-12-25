06f95d3d104e190fb394f1e495443db6ee679a8a
Aya Imanishi <konsaisai@gmail.com>
2022-12-25 13:31:07 +0900

    refactor

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -12,6 +12,9 @@ class Board {
         myBoard = board;
     }
 
+    public  int getLength(){
+        return  myBoard.length;
+    }
     public void showBoard(){
         System.out.println("----------");
         for (Cell[] row: myBoard) {  //拡張For文
@@ -19,7 +22,7 @@ class Board {
                 switch (x) {
                     case A -> System.out.print("|A");
                     case B -> System.out.print("|B");
-                    default -> System.out.print("|　");
+                    default -> System.out.print("| ");
                 }
             }
             System.out.println("|");
@@ -53,9 +56,8 @@ class Board {
             }
         }
 
-
         //横のチェック
-        for (int i = 0; i < myBoard[0].length; i++) {
+        for (int i = 0; i < myBoard[0].length - 1; i++) {
             val = myBoard[0][i];
             bEnd = true;
             for (Cell[] cells : myBoard) {
@@ -69,10 +71,30 @@ class Board {
             }
         }
 
-        //斜めのチェック
-        if (((myBoard[0][0] == myBoard[1][1]) && (myBoard[0][0] == myBoard[2][2])) ||
-                ((myBoard[0][2] == myBoard[1][1]) && (myBoard[0][2] == myBoard[2][0]))) {
-            return myBoard[0][0];
+        //斜めのチェック(右下がり)
+        val = myBoard[0][0];
+        bEnd = true;
+        for (int i = 1; i < myBoard.length; i++) {
+            if (val != myBoard[i][i]) {
+                bEnd = false;
+                break;
+            }
+        }
+        if (bEnd) {
+            return val;
+        }
+
+        //斜めのチェック(左下がり)
+        val = myBoard[(myBoard.length - 1)][0];
+        bEnd = true;
+        for (int i = myBoard.length; i > 0; i--) {
+            if (val != myBoard[(myBoard.length - i)][i]) {
+                bEnd = false;
+                break;
+            }
+        }
+        if (bEnd) {
+            return val;
         }
         return Cell.Nothing;
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -27,13 +27,16 @@ public class Main {
     public static void main(String[] args) {
         String red    = "\u001b[00;31m";
         String cyan   = "\u001b[00;36m";
+        int row = 5;
+        int col = 5;
 
-        Board board = new Board(3, 3);
+        //Board board = new Board(3, 3);
+        Board board = new Board(row, col);
         System.out.println("TicTacToe！！" + cyan);
         board.showBoard();
 
         //入力を取得してboardに反映する
-        for (int b =0; b < 9; b++) {
+        for (int b =0; b < (row * col); b++) {
             Cell type;
             if (b % 2 == 0) {
                 type = Cell.A;
@@ -49,7 +52,7 @@ public class Main {
                 Scanner in = new Scanner(System.in);
                 String num = in.nextLine();
                 try {
-                    position = parsePosition(num);
+                    position = parsePosition(num, board.getLength());
                 } catch (IllegalArgumentException e) {
                     System.out.println("Input Error!!!" + red);
                     System.out.println(e.getMessage());
@@ -76,20 +79,21 @@ public class Main {
 
 
     // 引数で受け取った文字列が数値かどうか正規表現でチェックするメソッド
-    public static Position parsePosition(String input) throws IllegalArgumentException {
+    public static Position parsePosition(String input, int len) throws IllegalArgumentException {
         String[] numbs = input.split(" ");
         if (numbs.length != 2) {
             throw new IllegalArgumentException ("行と列を半角スペースで区切ってください");
         }
 
-        Pattern pattern = Pattern.compile("([1-3])");
+        String regex = "([1-" + len + "])";
+        Pattern pattern = Pattern.compile(regex);
         if (!pattern.matcher(numbs[0]).matches()) {
             System.out.println("Input Error!!!");
-            throw new IllegalArgumentException("行は１から３で入力してください");
+            throw new IllegalArgumentException("行は1から" + len + "で入力してください");
         }
 
         if (!pattern.matcher(numbs[1]).matches()) {
-            throw new IllegalArgumentException("列は１から３で入力してください");
+            throw new IllegalArgumentException("行は1から" + len + "で入力してください");
         }
         return new Position(Integer.parseInt(numbs[0]) - 1, Integer.parseInt(numbs[1]) - 1);
     }
