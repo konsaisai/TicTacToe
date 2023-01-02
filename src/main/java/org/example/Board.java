75617c8ecdbcc8249597529a9a47d88ec17c03a2
Aya Imanishi <konsaisai@gmail.com>
2023-01-02 15:40:41 +0900

    refactor

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -42,20 +42,19 @@ class Board {
         for (int i = 0; i < myBoard.length; i ++) {
             for (int j = 0; j < myBoard[0].length; j ++){
                 //縦のチェック
-                //TODO 起点をメインのセルにする
-                if (checkLine(i - 1, j, 1, 0, 3)) {
+                if (checkLine(i, j, 1, 0, 3)) {
                     return myBoard[i][j];
                 }
                 //横のチェック
-                if (checkLine(i, j - 1, 0, 1, 3)) {
+                if (checkLine(i, j, 0, 1, 3)) {
                     return myBoard[i][j];
                 }
                 //斜め（右下がり）のチェック
-                if (checkLine(i - 1, j - 1, 1, 1, 3)) {
+                if (checkLine(i, j, 1, 1, 3)) {
                     return myBoard[i][j];
                 }
                 //斜め（左下がり）のチェック
-                if (checkLine(i - 1, j + 1, 1, -1, 3)) {
+                if (checkLine(i, j, 1, -1, 3)) {
                     return myBoard[i][j];
                 }
             }
