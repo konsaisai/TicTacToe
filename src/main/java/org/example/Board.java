cc3d4a903e744a2663aef08eb69d0be289ad9ba3
Aya Imanishi <konsaisai@gmail.com>
2023-01-02 15:28:07 +0900

    validate

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -39,29 +39,23 @@ class Board {
     }
 
     public Cell checkBoard() {
-        boolean bEnd;
-        Cell val;
-
         for (int i = 0; i < myBoard.length; i ++) {
             for (int j = 0; j < myBoard[0].length; j ++){
-
                 //縦のチェック
-                if (checkBoardEx(i - 1, j, 1, 0, 3)) {
+                //TODO 起点をメインのセルにする
+                if (checkLine(i - 1, j, 1, 0, 3)) {
                     return myBoard[i][j];
                 }
-
                 //横のチェック
-                if (checkBoardEx(i, j - 1, 0, 1, 3)) {
+                if (checkLine(i, j - 1, 0, 1, 3)) {
                     return myBoard[i][j];
                 }
-
                 //斜め（右下がり）のチェック
-                if (checkBoardEx(i - 1, j - 1, 1, 1, 3)) {
+                if (checkLine(i - 1, j - 1, 1, 1, 3)) {
                     return myBoard[i][j];
                 }
-
                 //斜め（左下がり）のチェック
-                if (checkBoardEx(i - 1, j + 1, 1, -1, 3)) {
+                if (checkLine(i - 1, j + 1, 1, -1, 3)) {
                     return myBoard[i][j];
                 }
             }
@@ -69,24 +63,29 @@ class Board {
         return Cell.Nothing;
     }
 
-    public boolean checkBoardEx(int row, int col, int rowInc, int colInc, int count) {
+    private boolean checkLine(int row, int col, int rowInc, int colInc, int count) {
         int ckRow = row;
         int ckCol = col;
-        try {
-            for (int i = 0; i < count - 1; i++) {
-                if (myBoard[row][col] == Cell.Nothing) {
-                    return false;
-                }
-                ckRow = ckRow + rowInc;
-                ckCol = ckCol + colInc;
-                if (myBoard[row][col] != myBoard[ckRow][ckCol]) {
-                    return false;
-                }
+        for (int i = 0; i < count - 1; i++) {
+            if (validateNumber(row) || validateNumber(col)) {
+                return false;
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            return false;
+            if (myBoard[row][col] == Cell.Nothing) {
+                return false;
+            }
+            ckRow = ckRow + rowInc;
+            ckCol = ckCol + colInc;
+            if (validateNumber(ckRow) || validateNumber(ckCol)) {
+                return false;
+            }
+            if (myBoard[row][col] != myBoard[ckRow][ckCol]) {
+                return false;
+            }
         }
         return true;
     }
 
+    private boolean validateNumber(int num){
+        return 0 > num || num >= myBoard.length;
+    }
 }
