35636df8dfaeed8d96e6fb332f2d3d5232154247
Aya Imanishi <konsaisai@gmail.com>
2023-06-04 22:35:10 +0900

    parsePositionTest add

---
+++ src/test/java/org/example/TicTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.example.Main.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicTacToeTest {
+
+    @Test
+    void ç() {
+        Position position = parsePosition("1 2", 8);
+        assertEquals(0, position.myRow);
+        assertEquals(1, position.myCol);
+
+        assertThrows(IllegalArgumentException.class, ()-> parsePosition("5 8", 3));
+        assertThrows(IllegalArgumentException.class, ()-> parsePosition("8", 1));
+        assertThrows(IllegalArgumentException.class, ()-> parsePosition("あ", 3));
+
+    }
+}
