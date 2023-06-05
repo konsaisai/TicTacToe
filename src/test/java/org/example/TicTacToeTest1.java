27a362a7785cb25d3ad2e7ba477d8621195a6322
aya.imanishi <konsaisai@gmail.com>
2023-06-05 13:55:08 +0900

    showBoardTest add

--- src/test/java/org/example/TicTacToeTest1.java
+++ src/test/java/org/example/TicTacToeTest1.java
@@ -2,6 +2,9 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.example.Main.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,7 +29,29 @@ class TicTacToeTest1 {
 
     @Test
     void showBoardTest() {
+        PrintStream standardOut = System.out;
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        // テスト対象のメソッド呼び出し
+        Board board = new Board(3, 3);
+        board.changeBoard(Main.parsePosition("1 1", 3), Cell.A);
+        board.changeBoard(Main.parsePosition("2 3", 3), Cell.A);
+        board.changeBoard(Main.parsePosition("1 2", 3), Cell.B);
+        board.changeBoard(Main.parsePosition("2 2", 3), Cell.B);
+
+        board.showBoard();
+        // 期待する出力を定義
+        String expectedOutput = "----------\r\n" +
+                "|A|B| |\r\n" +
+                "| |B|A|\r\n" +
+                "| | | |\r\n" +
+                "----------\r\n";
+
+        // 期待する出力と実際の出力を比較
+        assertEquals(expectedOutput, outputStreamCaptor.toString());
+        System.setOut(standardOut);
     }
 
 }
